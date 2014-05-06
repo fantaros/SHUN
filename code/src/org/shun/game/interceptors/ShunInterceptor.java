3330b63fded72a7a6ecdcc9cdc171c5ef121e567
fantaros <fantasymon@gmail.com>
2014-05-06 23:14:55 +0800

    ignore build folder
    Signed-off-by: fantaros <fantasymon@gmail.com>

--- code/src/org/shun/game/interceptors/ShunInterceptor.java
+++ code/src/org/shun/game/interceptors/ShunInterceptor.java
@@ -1,5 +1,34 @@
 package org.shun.game.interceptors;
 
-public class ShunInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ShunInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
